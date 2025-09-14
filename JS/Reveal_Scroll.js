16ca3c54fb38e78eee50c47cfc24646ce6c6f7e8
bileromerson <158579506+bileromerson@users.noreply.github.com>
2025-09-14 18:35:01 -0300

    finish index

--- JS/Reveal_Scroll.js
+++ JS/Reveal_Scroll.js
@@ -1,12 +1,10 @@
-// Seleciona TODOS os elementos com a classe 'hidden'
+
 const hiddenElements = document.querySelectorAll('.hidden');
 
 // Cria um observador
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    // entry.target é o elemento que o observador está vendo agora
     if (entry.isIntersecting) {
-      // Se ele está visível, adiciona a classe 'visible'
       entry.target.classList.add('visible');
     }
     else{
@@ -17,5 +15,4 @@ const observer = new IntersectionObserver((entries) => {
   threshold: 0.5 
 });
 
-// Para cada elemento na lista, comece a observá-lo
 hiddenElements.forEach((el) => observer.observe(el));
--- JS/Text.js
+++ JS/Text.js
@@ -1,16 +1,14 @@
 
 
-
 function list(){
     const listaDeItens = [
-        "Connection sucessfull: Url",
+        "Connection sucessfull",
         "Extract data...",
-        "Text",
-        "Text",
-        "Text",
-        "Text",
-        "Text",
-        "Text",
+        "some contents were found:",
+        "   [1] About Me",
+        "   [2] Interests",
+        "   [3] Projects",
+        "   [4] Contacts",
     ];
 
     const listaElemento = document.getElementById('list');
@@ -26,7 +24,15 @@ function list(){
             // Opcional: Adiciona uma classe para uma animação suave
             setTimeout(() => {
                 novoItem.classList.add('fade-in');
-            }, 10);
+            }, 100);
+            if (index === listaDeItens.length - 1) {
+                setTimeout(() => {
+                    const input = document.querySelector('.appear');
+                    if (input) {
+                        input.style.display = 'block';
+                    }
+                }, 400); // espera um pouco para a animação terminar
+            }
             
         }, atrasoEntreItens * (index + 1));
     });
@@ -35,7 +41,7 @@ function list(){
 let indice = 0;
 function digitar(){
     const elemento = document.getElementById('texto-digitado');
-    const textoCompleto = "Hi There!!! I'm Bileromerson";
+    const textoCompleto = "Hi There! I'm Bileromerson";
     elemento.textContent += textoCompleto[indice];
     indice++;
     if (indice < textoCompleto.length) {
