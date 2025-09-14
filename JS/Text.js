d96573990bd31e3430f2be402fc0afdf5c499075
bileromerson <158579506+bileromerson@users.noreply.github.com>
2025-09-14 13:12:37 -0300

    input(inicio)

--- JS/Text.js
+++ JS/Text.js
@@ -1,49 +1,49 @@
 
 
+
+function list(){
+    const listaDeItens = [
+        "Connection sucessfull: Url",
+        "Extract data...",
+        "Text",
+        "Text",
+        "Text",
+        "Text",
+        "Text",
+        "Text",
+    ];
+
+    const listaElemento = document.getElementById('list');
+
+    const atrasoEntreItens = 300; 
+
+    listaDeItens.forEach((textoDoItem, index) => {
+        setTimeout(() => {
+            const novoItem = document.createElement('li');
+            novoItem.textContent = textoDoItem;
+            listaElemento.appendChild(novoItem);
+            
+            // Opcional: Adiciona uma classe para uma animação suave
+            setTimeout(() => {
+                novoItem.classList.add('fade-in');
+            }, 10);
+            
+        }, atrasoEntreItens * (index + 1));
+    });
+}
+
 let indice = 0;
 function digitar(){
     const elemento = document.getElementById('texto-digitado');
-    const textoCompleto = "Hi There!!!";
+    const textoCompleto = "Hi There!!! I'm Bileromerson";
     elemento.textContent += textoCompleto[indice];
     indice++;
     if (indice < textoCompleto.length) {
         setTimeout(digitar, 90);
     }
+    else{
+    list()
+    }
 }
+
 digitar()
-
-// O texto de cada item da lista
-const listaDeItens = [
-    "Text",
-    "Text",
-    "Text",
-    "Text",
-    "Text",
-    "Text",
-    "Text",
-    "Text",
-];
-
-const listaElemento = document.getElementById('list');
-
-const atrasoEntreItens = 300; 
-
-listaDeItens.forEach((textoDoItem, index) => {
-    // Para cada item, programa uma ação com um atraso
-    setTimeout(() => {
-        // Cria um novo elemento <li>
-        const novoItem = document.createElement('li');
-        
-        // Define o texto do novo <li>
-        novoItem.textContent = textoDoItem;
-        
-        // Adiciona o novo <li> à lista <ul>
-        listaElemento.appendChild(novoItem);
-        
-        // Opcional: Adiciona uma classe para uma animação suave
-        setTimeout(() => {
-            novoItem.classList.add('fade-in');
-        }, 10);
-        
-    }, atrasoEntreItens * (index + 1));
-});
